8d98a53b78af8ae2c4a0717d2da2137129f7bebb
Oscar Montes <oscmcojc1@live.com.mx>
2019-08-21 19:11:19 -0500

    Configuring getHours from client side

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,4 +1,3 @@
-const axios = require('axios');
 const admin = require("firebase-admin");
 const serviceAccount = require("../config/firebase-key.json");
 
@@ -8,44 +7,62 @@ admin.initializeApp({
 const db = admin.firestore();
 
 //CONFIG DATES
-// db.doc('dates/20septiembre').set({
-//    availableHours: ['8:00 a.m.', '8:20 a.m.', '8:40 a.m.', '9:00 a.m.', '9:20 a.m.', '9:40 a.m.',
-//        '10:00 a.m.', '10:20 a.m.', '10:40 a.m.',
-//        '11:00 a.m.', '11:20 a.m.', '11:40 a.m.'],
-//     name: '20 de septiembre'
-// });
-// db.doc('dates/20septiembre').collection('8:00').doc('additionalInfo').set({additionalInfo: ''});
-// db.doc('dates/20septiembre').collection('8:20').doc('additionalInfo').set({additionalInfo: ''});
-// db.doc('dates/20septiembre').collection('8:40').doc('additionalInfo').set({additionalInfo: ''});
-// db.doc('dates/20septiembre').collection('9:00').doc('additionalInfo').set({additionalInfo: ''});
-// db.doc('dates/20septiembre').collection('9:20').doc('additionalInfo').set({additionalInfo: ''});
-// db.doc('dates/20septiembre').collection('9:40').doc('additionalInfo').set({additionalInfo: ''});
-// db.doc('dates/20septiembre').collection('10:00').doc('additionalInfo').set({additionalInfo: ''});
-// db.doc('dates/20septiembre').collection('10:20').doc('additionalInfo').set({additionalInfo: ''});
-// db.doc('dates/20septiembre').collection('10:40').doc('additionalInfo').set({additionalInfo: ''});
-// db.doc('dates/20septiembre').collection('11:00').doc('additionalInfo').set({additionalInfo: ''});
-// db.doc('dates/20septiembre').collection('11:20').doc('additionalInfo').set({additionalInfo: ''});
-// db.doc('dates/20septiembre').collection('11:40').doc('additionalInfo').set({additionalInfo: ''});
+db.collection('dates').doc('availableHours').set({
+    '20 de septiembre': ['8:00 a.m.', '8:20 a.m.', '8:40 a.m.',
+            '9:00 a.m.', '9:20 a.m.', '9:40 a.m.',
+            '10:00 a.m.', '10:20 a.m.', '10:40 a.m.',
+            '11:00 a.m.', '11:20 a.m.', '11:40 a.m.',
+            '12:00 p.m.', '12:20 p.m.', '12:40 p.m.'],
+    '21 de septiembre': ['8:00 a.m.', '8:20 a.m.', '8:40 a.m.',
+            '9:00 a.m.', '9:20 a.m.', '9:40 a.m.',
+            '10:00 a.m.', '10:20 a.m.', '10:40 a.m.',
+            '11:00 a.m.', '11:20 a.m.', '11:40 a.m.',
+            '12:00 p.m.', '12:20 p.m.', '12:40 p.m.']
+});
+// db.doc('dates/20 de septiembre').collection('8:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('8:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('8:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('9:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('9:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('9:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('10:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('10:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('10:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('11:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('11:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('11:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('12:00 p.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('12:20 p.m.').doc('additionalInfo').set({additionalInfo: ''});
+// db.doc('dates/20 de septiembre').collection('12:40 p.m.').doc('additionalInfo').set({additionalInfo: ''});
 
 module.exports = (app, passport) => {
-    //agregar validaciones
     const usersCollection = db.collection('users');
+    app.get('/registro', (req, res) => {
+        db.collection('dates').doc('availableHours').get().then(doc => {
+            // console.log(Object.keys(doc.data()));
+            // console.log(Object.entries(doc.data())[1][1]);
+            res.render('registro', {
+                dates: doc.data()
+            });
+        });
+    });
     app.post('/register', (req, res) => {
-        if (req.body.nua.length !== 6 || req.body.first_name.length <= 2) {
-            return res.send('Invalid input');
-        }
         try {
+            const day = req.body.day.replace('-', ' ').replace('-', ' ');
+            if (req.body.nua.length !== 6 || req.body.first_name.length <= 2) {
+                return res.send('Invalid input');
+            }
             const userDoc = usersCollection.doc(req.body.nua);
             userDoc.get().then(doc => {
                 if (doc.exists) {
                     return Promise.reject('User already exists');
                 }
             }).then(() => {
-                return db.doc(`dates/${req.body.date}`).collection(req.body.time).get().then(snap => {
+                return db.doc(`dates/${day}`).collection(req.body.time).get().then(snap => {
                     return snap.size;
                 });
             }).then((size) => {
-                const dateRef = db.doc(`/dates/${req.body.date}`);
+                const dateRef = db.doc(`/dates/${day}`);
                 if (size >= 7) {
                     return Promise.reject('There are not more beds available for this hour.');
                 } else if (size === 6) {
@@ -54,7 +71,6 @@ module.exports = (app, passport) => {
                         return res.data().availableHours.filter(value => {
                             return value !== req.body.time;
                         });
-
                     }).then(newHoursList => {
                         dateRef.update({
                             availableHours: newHoursList
@@ -70,51 +86,25 @@ module.exports = (app, passport) => {
                     first_name: req.body.first_name,
                     last_name: req.body.last_name,
                     email: req.body.email,
-                    date: req.body.date,
+                    date: day,
                     hour: req.body.time,
                     career: req.body.career
                 }).catch(error => {
                     console.error('Failed to update hour collection', error);
                 });
             }).then(() => {
-                console.log('User registered.');
-                res.send('Registered')
+                res.send('Registered');
             }).catch(error => {
                 res.send(error);
             })
-        } catch (error) {
-            console.error('Error while registering user:', error);
+        } catch (e) {
+            console.error('CATCH:', e);
         }
-    });
-    app.post('/login', (req, res) => {
-        usersCollection.doc(req.body.nua).get().then(doc => {
-            if (!doc.exists) {
-                //el nua no existe
-                console.log('user not found');
-            } else if (doc.data().password === req.body.password) {
-                //login correcto
-                console.log(doc.data());
-            } else {
-                //pass incorrecta
-                res.send('incorrect pass');
-            }
-        }).catch(error => {
-            console.error('Error getting document', error);
-        });
-    });
 
-    app.get('/days', (req, res) => {
-        db.collection('dates').get().then(snap => {
-            const datesList = snap.docs.map(doc => {
-                return doc.data().name;
-            });
-            res.json(datesList);
-        });
     });
-
-    app.post('/hours', (req, res) => {
-        db.doc(`dates/${req.body.day}`).get().then(hoursList => {
-            res.json(hoursList.data().availableHours);
+    app.get('/hours', (req, res) => {
+        db.collection(`dates`).doc('availableHours').get().then(hoursList => {
+            res.json(hoursList.data());
         });
     });
 };
