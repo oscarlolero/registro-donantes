0406280c926a346ed8767dafd1df6b563885a843
Oscar Montes <oscmcojc1@live.com.mx>
2019-08-21 22:43:27 -0500

    Firestore database and some routes restructured

--- src/app/routes.js
+++ src/app/routes.js
@@ -7,18 +7,18 @@ admin.initializeApp({
 const db = admin.firestore();
 
 //CONFIG DATES
-db.collection('dates').doc('availableHours').set({
-    '20 de septiembre': ['8:00 a.m.', '8:20 a.m.', '8:40 a.m.',
-            '9:00 a.m.', '9:20 a.m.', '9:40 a.m.',
-            '10:00 a.m.', '10:20 a.m.', '10:40 a.m.',
-            '11:00 a.m.', '11:20 a.m.', '11:40 a.m.',
-            '12:00 p.m.', '12:20 p.m.', '12:40 p.m.'],
-    '21 de septiembre': ['8:00 a.m.', '8:20 a.m.', '8:40 a.m.',
-            '9:00 a.m.', '9:20 a.m.', '9:40 a.m.',
-            '10:00 a.m.', '10:20 a.m.', '10:40 a.m.',
-            '11:00 a.m.', '11:20 a.m.', '11:40 a.m.',
-            '12:00 p.m.', '12:20 p.m.', '12:40 p.m.']
-});
+// db.collection('dates').doc('availableHours').set({
+//     '20 de septiembre': ['8:00 a.m.', '8:20 a.m.', '8:40 a.m.',
+//             '9:00 a.m.', '9:20 a.m.', '9:40 a.m.',
+//             '10:00 a.m.', '10:20 a.m.', '10:40 a.m.',
+//             '11:00 a.m.', '11:20 a.m.', '11:40 a.m.',
+//             '12:00 p.m.', '12:20 p.m.', '12:40 p.m.'],
+//     '21 de septiembre': ['8:00 a.m.', '8:20 a.m.', '8:40 a.m.',
+//             '9:00 a.m.', '9:20 a.m.', '9:40 a.m.',
+//             '10:00 a.m.', '10:20 a.m.', '10:40 a.m.',
+//             '11:00 a.m.', '11:20 a.m.', '11:40 a.m.',
+//             '12:00 p.m.', '12:20 p.m.', '12:40 p.m.']
+// });
 // db.doc('dates/20 de septiembre').collection('8:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
 // db.doc('dates/20 de septiembre').collection('8:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
 // db.doc('dates/20 de septiembre').collection('8:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
@@ -34,7 +34,7 @@ db.collection('dates').doc('availableHours').set({
 // db.doc('dates/20 de septiembre').collection('12:00 p.m.').doc('additionalInfo').set({additionalInfo: ''});
 // db.doc('dates/20 de septiembre').collection('12:20 p.m.').doc('additionalInfo').set({additionalInfo: ''});
 // db.doc('dates/20 de septiembre').collection('12:40 p.m.').doc('additionalInfo').set({additionalInfo: ''});
-
+// db.doc('dates/20 de septiembre').set({exists: true});
 module.exports = (app, passport) => {
     const usersCollection = db.collection('users');
     app.get('/registro', (req, res) => {
@@ -47,8 +47,9 @@ module.exports = (app, passport) => {
         });
     });
     app.post('/register', (req, res) => {
+        console.log(req.body);
         try {
-            const day = req.body.day.replace('-', ' ').replace('-', ' ');
+            const day = req.body.day;
             if (req.body.nua.length !== 6 || req.body.first_name.length <= 2) {
                 return res.send('Invalid input');
             }
@@ -58,28 +59,28 @@ module.exports = (app, passport) => {
                     return Promise.reject('User already exists');
                 }
             }).then(() => {
-                return db.doc(`dates/${day}`).collection(req.body.time).get().then(snap => {
+                return db.doc(`dates/${day}`).collection(req.body.hour).get().then(snap => {
                     return snap.size;
                 });
             }).then((size) => {
-                const dateRef = db.doc(`/dates/${day}`);
+                const dateRef = db.doc(`dates/${day}`);
                 if (size >= 7) {
                     return Promise.reject('There are not more beds available for this hour.');
                 } else if (size === 6) {
                     //En caso de llenarse las 6 camas, remover el horario de la lista
-                    dateRef.get().then(res => {
-                        return res.data().availableHours.filter(value => {
-                            return value !== req.body.time;
+                    db.collection('dates').doc('availableHours').get().then(res => {
+                        return res.data()[day].filter(value => {
+                            return value !== req.body.hour;
                         });
                     }).then(newHoursList => {
-                        dateRef.update({
-                            availableHours: newHoursList
+                        db.collection('dates').doc('availableHours').update({
+                            [day]: newHoursList
                         }).catch(error => {
                             console.error('Failed to update availableHours', error);
                         });
                     });
                 }
-                dateRef.collection(req.body.time).doc(req.body.nua).set({additionalInfo: ''}).catch(error => {
+                dateRef.collection(req.body.hour).doc(req.body.nua).set({additionalInfo: ''}).catch(error => {
                     console.error('Failed to update hour collection', error);
                 });
                 userDoc.set({
@@ -87,8 +88,9 @@ module.exports = (app, passport) => {
                     last_name: req.body.last_name,
                     email: req.body.email,
                     date: day,
-                    hour: req.body.time,
-                    career: req.body.career
+                    hour: req.body.hour,
+                    career: req.body.career,
+                    assist: false
                 }).catch(error => {
                     console.error('Failed to update hour collection', error);
                 });
