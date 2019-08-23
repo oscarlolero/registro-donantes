c930de220ad143e8ac0f49dee05fe7e083df54b5
Oscar Montes <oscmcojc1@live.com.mx>
2019-08-23 02:08:24 -0500

    Restructured all requests to async/await, added 'registrado' route

--- src/app/routes.js
+++ src/app/routes.js
@@ -35,78 +35,82 @@ const db = admin.firestore();
 // db.doc('dates/20 de septiembre').collection('12:20 p.m.').doc('additionalInfo').set({additionalInfo: ''});
 // db.doc('dates/20 de septiembre').collection('12:40 p.m.').doc('additionalInfo').set({additionalInfo: ''});
 // db.doc('dates/20 de septiembre').set({exists: true});
-module.exports = (app, passport) => {
-    const usersCollection = db.collection('users');
+module.exports = (app) => {
+    //Páginas
     app.get('/registro', (req, res) => {
-        db.collection('dates').doc('availableHours').get().then(doc => {
-            // console.log(Object.keys(doc.data()));
-            // console.log(Object.entries(doc.data())[1][1]);
-            res.render('registro', {
-                dates: doc.data()
-            });
+        res.render('registro', {
+            registerMsg: req.flash('registerMsg')
         });
     });
-    app.post('/register', (req, res) => {
-        console.log(req.body);
+    app.get('/registrado', async (req, res) => {
+        //req.flash('NUA');
+        res.render('registrado');
+    });
+
+    //Validaciones y registros
+    app.post('/register', async (req, res) => {
         try {
+            let data;
             const day = req.body.day;
-            if (req.body.nua.length !== 6 || req.body.first_name.length <= 2) {
-                return res.send('Invalid input');
+            const userDoc = db.collection('users').doc(req.body.nua);
+            //Verificar que las entradas son válidas
+            if (req.body.nua.length !== 6 || req.body.first_name.length < 3 || req.body.last_name.length < 3
+                || req.body.email < 4 || req.body.hour === 0 || req.body.day === 0 || typeof req.body.hour === 'undefined') {
+                req.flash('registerMsg', 'Verifica que has ingresado todos los datos de manera correcta.');
+                return res.redirect('/registro');
             }
-            const userDoc = usersCollection.doc(req.body.nua);
-            userDoc.get().then(doc => {
-                if (doc.exists) {
-                    return Promise.reject('User already exists');
-                }
-            }).then(() => {
-                return db.doc(`dates/${day}`).collection(req.body.hour).get().then(snap => {
-                    return snap.size;
+            //Checar si el NUA existe
+            data = await userDoc.get();
+            if(data.exists) {
+                req.flash('registerMsg', 'Este NUA ya está registrado.');
+                return res.redirect('/registro');
+            }
+            //Chcar los lugares disponibles
+            data = await db.doc(`dates/${day}`).collection(req.body.hour).get();
+            if(data.size >= 7) {
+                req.flash('registerMsg', 'Se llenó el horario que escogiste.');
+                return res.redirect('/registro');
+            } else if(data.size === 6) {
+                //En caso de llenarse las 6 camas, remover el horario de la lista
+                const hoursData = await db.collection('dates').doc('availableHours').get();
+                const newHoursList = hoursData.data()[day].filter(value => {
+                    return value !== req.body.hour;
                 });
-            }).then((size) => {
-                const dateRef = db.doc(`dates/${day}`);
-                if (size >= 7) {
-                    return Promise.reject('There are not more beds available for this hour.');
-                } else if (size === 6) {
-                    //En caso de llenarse las 6 camas, remover el horario de la lista
-                    db.collection('dates').doc('availableHours').get().then(res => {
-                        return res.data()[day].filter(value => {
-                            return value !== req.body.hour;
-                        });
-                    }).then(newHoursList => {
-                        db.collection('dates').doc('availableHours').update({
-                            [day]: newHoursList
-                        }).catch(error => {
-                            console.error('Failed to update availableHours', error);
-                        });
-                    });
-                }
-                dateRef.collection(req.body.hour).doc(req.body.nua).set({additionalInfo: ''}).catch(error => {
-                    console.error('Failed to update hour collection', error);
+                await db.collection('dates').doc('availableHours').update({
+                    [day]: newHoursList
                 });
-                userDoc.set({
-                    first_name: req.body.first_name,
-                    last_name: req.body.last_name,
-                    email: req.body.email,
-                    date: day,
-                    hour: req.body.hour,
-                    career: req.body.career,
-                    assist: false
-                }).catch(error => {
-                    console.error('Failed to update hour collection', error);
-                });
-            }).then(() => {
-                res.send('Registered');
-            }).catch(error => {
-                res.send(error);
-            })
+            }
+            //Registrar el NUA en la colección de su horario
+            db.doc(`dates/${day}`).collection(req.body.hour).doc(req.body.nua).set({additionalInfo: ''});
+            await userDoc.set({
+                first_name: req.body.first_name,
+                last_name: req.body.last_name,
+                email: req.body.email,
+                date: day,
+                hour: req.body.hour,
+                career: req.body.career,
+                assist: false
+            });
+            req.flash('NUA', req.body.nua);
+            res.redirect('registrado');
         } catch (e) {
+            req.flash('registerMsg', 'Hubo un error interno con el servidor.');
+            res.render('registro', {
+                message: req.flash('registerMsg')
+            });
             console.error('CATCH:', e);
         }
-
     });
+
+    //Consultas
     app.get('/hours', (req, res) => {
         db.collection(`dates`).doc('availableHours').get().then(hoursList => {
             res.json(hoursList.data());
+        });
+    });
+    app.get('/days', (req, res) => {
+        db.collection('dates').doc('availableHours').get().then(doc => {
+            res.json(doc.data());
         });
     });
 };
--- src/server.js
+++ src/server.js
@@ -2,14 +2,11 @@ const express = require('express');
 const app = express();
 
 const path = require('path'); //manejar rutas/direcciones del servidor
-const passport = require('passport');
 const flash = require('connect-flash');
 const morgan = require('morgan'); //logeo de peticiones http
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser'); //procesar información desde el navegador al servidor
 const session = require('express-session');
-
-require('./config/passport')(passport); //Configurar passsport
 
 //Configuraciones
 app.set('port', process.env.PORT || 3000);
@@ -27,12 +24,9 @@ app.use(session({
     resave: false,
     saveUninitialized: false
 }));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use(flash());
-
 //Rutas
-require('./app/routes')(app, passport);
+require('./app/routes')(app);
 
 //Archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
