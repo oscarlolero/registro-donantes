f66f16de5fe8ef45067afee07fc3caf336a57b3d
oscmcojc1@live.com.mx <oscar6932124>
2019-08-26 19:51:31 -0500

    New dates

--- src/config/db.js
+++ src/config/db.js
@@ -26,12 +26,12 @@ exports.getAssistace = async () => {
 //Crear registros en base de datos
 exports.cleanAndPopulateDB = () => {
     db.collection('dates').doc('availableHours').set({
-        '20 de septiembre': ['8:00 a.m.', '8:20 a.m.', '8:40 a.m.',
+        '3 de septiembre': ['8:00 a.m.', '8:20 a.m.', '8:40 a.m.',
             '9:00 a.m.', '9:20 a.m.', '9:40 a.m.',
             '10:00 a.m.', '10:20 a.m.', '10:40 a.m.',
             '11:00 a.m.', '11:20 a.m.', '11:40 a.m.',
             '12:00 p.m.', '12:20 p.m.', '12:40 p.m.'],
-        '21 de septiembre': ['8:00 a.m.', '8:20 a.m.', '8:40 a.m.',
+        '4 de septiembre': ['8:00 a.m.', '8:20 a.m.', '8:40 a.m.',
             '9:00 a.m.', '9:20 a.m.', '9:40 a.m.',
             '10:00 a.m.', '10:20 a.m.', '10:40 a.m.',
             '11:00 a.m.', '11:20 a.m.', '11:40 a.m.',
@@ -40,37 +40,37 @@ exports.cleanAndPopulateDB = () => {
     db.collection('stats').doc('dbStats').set({
         usersRegistered: 0
     });
-    db.doc('dates/20 de septiembre').collection('8:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('8:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('8:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('9:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('9:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('9:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('10:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('10:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('10:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('11:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('11:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('11:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('12:00 p.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('12:20 p.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').collection('12:40 p.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/20 de septiembre').set({exists: true});
-    //21 DE SEPTIEMBRE
-    db.doc('dates/21 de septiembre').collection('8:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('8:21 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('8:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('9:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('9:21 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('9:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('10:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('10:21 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('10:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('11:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('11:21 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('11:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('12:00 p.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('12:21 p.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').collection('12:40 p.m.').doc('additionalInfo').set({additionalInfo: ''});
-    db.doc('dates/21 de septiembre').set({exists: true});
+    db.doc('dates/3 de septiembre').collection('8:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('8:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('8:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('9:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('9:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('9:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('10:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('10:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('10:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('11:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('11:20 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('11:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('12:00 p.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('12:20 p.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').collection('12:40 p.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/3 de septiembre').set({exists: true});
+    //4 de septiembre
+    db.doc('dates/4 de septiembre').collection('8:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('8:21 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('8:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('9:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('9:21 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('9:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('10:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('10:21 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('10:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('11:00 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('11:21 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('11:40 a.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('12:00 p.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('12:21 p.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').collection('12:40 p.m.').doc('additionalInfo').set({additionalInfo: ''});
+    db.doc('dates/4 de septiembre').set({exists: true});
 };
